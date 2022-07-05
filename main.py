db1076ed5d57f6e4402d99c9a53a069e6333455c
AMFeniksPL <arkadiuszmazurek13@gmail.com>
2022-07-05 23:26:04 +0200

    Big renovation in code

--- main.py
+++ main.py
@@ -3,6 +3,40 @@ import pygame
 from pygame.locals import *
 import random
 
+# CONSTANTS:
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 700
+LEFT_LIMIT = 100
+RIGHT_LIMIT = SCREEN_WIDTH - LEFT_LIMIT
+PLAYER_START_POSITION_Y = SCREEN_HEIGHT - 100
+
+# KEEP GAME IN MAX 60 FPS
+clock = pygame.time.Clock()
+FPS = 60
+
+# IMPROVE GAME SPEED DURING GAME:
+TIME_TO_ACCELERATE = 30
+lastAccelerate = time.time()
+
+# SCORE
+score = 0
+
+# OFFSET MOVEMENT
+offsetX = 0
+offsetY = 0
+offsetVelocityY = 0
+
+# IMAGES:
+leftWallImage = pygame.image.load('Images/LeftWall.png')
+rightWallImage = pygame.transform.flip(leftWallImage, True, False)
+backImage = pygame.image.load('Images/BackWall.png')
+
+pygame.init()
+fontPlatform = pygame.font.SysFont('Arial', 14)
+fontScore = pygame.font.SysFont('Arial', 48)
+hurryUpText = fontScore.render("HURRY UP!!!", True, (200, 200, 0))
+hurryUpTextPosY = -50
+
 
 class Player:
     def __init__(self, x, y):
@@ -13,6 +47,9 @@ class Player:
         self.velX = 0
         self.velY = 0
         self.canJump = False
+        self.angle = 0
+        self.jumping = False
+        self.rotating = False
 
     def move(self):
         self.velY += 0.5
@@ -33,10 +70,27 @@ class Player:
             self.x = RIGHT_LIMIT - self.image.get_width()
             self.velX *= -1
 
+    def check_collision(self, listOfPlatforms):
         self.rect = Rect(self.x, self.y, self.image.get_width(), self.image.get_height())
+        for platform in listOfPlatforms:
+            if self.rect.colliderect(platform.Rect) and self.velY > 0:
+                self.y = platform.y - self.image.get_height()
+                self.velY = 0
+                self.velX *= 0.95
+                self.canJump = True
+                self.rotating = False
+
+    def update(self):
+        self.move()
+        self.check_collision(platformList)
+
+        if self.rotating:
+            self.angle += 10
+        else:
+            self.angle = 0
 
     def draw(self):
-        screen.blit(self.image, (self.x + offsetX, self.y + offsetY))
+        screen.blit(pygame.transform.rotate(self.image, self.angle), (self.x + offsetX, self.y + offsetY))
 
 
 class Platform:
@@ -62,156 +116,117 @@ class Platform:
         screen.blit(self.image, (self.x + offsetX, self.y + offsetY))
 
         if self.number % 10 == 0:
+            centerPos = (self.x + self.image.get_width() / 2 + offsetX, self.y + self.image.get_height() / 2 + offsetY)
+            pygame.draw.rect(screen, (10, 10, 10), Rect(centerPos[0] - 10, centerPos[1] - 10, 30, 30))
             img1 = fontPlatform.render(str(self.number), True, (200, 200, 255))
-            screen.blit(img1,
-                        (self.x + self.image.get_width() / 2 + offsetX, self.y + self.image.get_height() / 2 + offsetY))
+            screen.blit(img1, centerPos)
+
+    @staticmethod
+    def make_another_platform():
+        posX = random.randint(10 + LEFT_LIMIT, RIGHT_LIMIT - Platform.image.get_width() - 10)
+        posY = PLAYER_START_POSITION_Y - 100 * Platform.counter
+        platformList.append(Platform(posX, posY))
+
+    @staticmethod
+    def make_first_platforms():
+        for i in range(11):
+            Platform.make_another_platform()
+
+    @staticmethod
+    def manage_platforms():
+        # Dodawanie platform
+        if player.y < 1500 - 100 * Platform.counter:
+            Platform.make_another_platform()
+
+        # Usuwanie platform
+        for platform in platformList:
+            if platform.y + offsetY > SCREEN_HEIGHT:
+                platformList.remove(platform)
+                global score
+                score += 10
+                del platform
 
 
-SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = 700
-
-LEFT_LIMIT = 100
-RIGHT_LIMIT = SCREEN_WIDTH - 100
-
-player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT - 100)
-
-# Rozpoczyna działanie PyGame
-pygame.init()
+class OffsetBackground:
+    @staticmethod
+    def draw(x, y, image, offsetScale=1.0):
+        screen.blit(image, (x, y + (offsetScale * offsetY) % image.get_height()))
+        screen.blit(image, (x, y - image.get_height() + ((offsetScale * offsetY) % image.get_height())))
 
 
-
-def keyboard_steering():
+def player_input():
     keys = pygame.key.get_pressed()
     if keys[K_UP] and player.canJump:
         player.velY = - (10 + 0.7 * abs(player.velX))
         player.canJump = False
+        if player.velY < -18:
+            player.rotating = True
+
     if keys[K_LEFT]:
         player.velX -= 1
     if keys[K_RIGHT]:
         player.velX += 1
 
-def player_movement():
-    player.move()
+
+def game_logic():
+    player.update()
+
 
 def draw_game():
     screen.fill((0, 0, 30))
 
-    screen.blit(backImage, (LEFT_LIMIT, (0.5 * offsetY) % backImage.get_height()))
-    screen.blit(backImage, (LEFT_LIMIT, -backImage.get_height() + ((0.5 * offsetY) % backImage.get_height())))
-
-    player.draw()
+    OffsetBackground.draw(LEFT_LIMIT, 0, backImage, 0.5)
 
     for platform in platformList:
         platform.draw()
 
-    screen.blit(leftWallImage, (0, (1.3 * offsetY) % leftWallImage.get_height()))
-    screen.blit(leftWallImage, (RIGHT_LIMIT, (1.3 * offsetY) % leftWallImage.get_height()))
+    player.draw()
 
-    screen.blit(leftWallImage, (0, -leftWallImage.get_height() + ((1.3 * offsetY) % leftWallImage.get_height())))
-    screen.blit(leftWallImage,
-                (RIGHT_LIMIT, -leftWallImage.get_height() + ((1.3 * offsetY) % leftWallImage.get_height())))
+    OffsetBackground.draw(0, 0, leftWallImage, 1.3)
+    OffsetBackground.draw(RIGHT_LIMIT, 0, rightWallImage, 1.3)
 
+    # RYSOWANIE INTERFEJSU
     img1 = fontScore.render(str(score), True, (200, 200, 0))
     screen.blit(img1, (20, 20))
     screen.blit(hurryUpText, (300, hurryUpTextPosY))
 
 
-
-
-fontPlatform = pygame.font.SysFont('Arial', 12)
-fontScore = pygame.font.SysFont('Arial', 48)
-
-score = 0
-
-# Dzięki tym dwóm linijkom mamy stałe 60 klatek na sekundę;
-clock = pygame.time.Clock()
-fps = 60
-
-timeToAccelerate = 30
-lastAccelerate = time.time()
-
-# Parametry okna zapisane do zmiennych:
-
-
+player = Player(SCREEN_WIDTH / 2, PLAYER_START_POSITION_Y)
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption('Gierka')
-
-
+pygame.display.set_caption('Icy Tower, ale to projekt testowy z Pygame')
 
 platformList = []
-
-for i in range(10):
-    posX = random.randint(10 + LEFT_LIMIT, RIGHT_LIMIT - Platform.image.get_width() - 10)
-    posY = (SCREEN_HEIGHT - 100) - 100 * Platform.counter
-    platformList.append(Platform(posX, posY))
-
-offsetX = 0
-offsetY = 0
-
-offsetVelocityY = 0
-
-leftWallImage = pygame.image.load('Images/LeftWall.png')
-backImage = pygame.image.load('Images/BackWall.png')
-
-hurryUpText = fontScore.render("HURRY UP!!!", True, (200, 200, 0))
-hurryUpTextPosY = -50
+Platform.make_first_platforms()
 
 run = True
 while run:
 
     # Zegar gry
-    clock.tick(fps)
-    keyboard_steering()
-    player_movement()
-
-
-
-    # Kolizje platformy i gracza
-
-    for platform in platformList:
-        if player.rect.colliderect(platform.Rect) and player.velY > 0:
-            player.y = platform.y - player.image.get_height()
-            player.velY = 0
-            player.velX *= 0.95
-            canJump = True
+    clock.tick(FPS)
+    player_input()
+    game_logic()
 
     # Przewijanie się ekranu po pierwszym większym skoku;
     if player.y < 100 and offsetY < 200:
         offsetVelocityY = 1
 
     # Przyspieszanie przewijania ekranu co określnony czas
-    if time.time() - lastAccelerate > timeToAccelerate and offsetVelocityY > 0:
+    if time.time() - lastAccelerate > TIME_TO_ACCELERATE and offsetVelocityY > 0:
         offsetVelocityY += 1
         lastAccelerate = time.time()
         hurryUpTextPosY = SCREEN_HEIGHT + 100
 
-
     offsetY += offsetVelocityY
 
-    if player.y + offsetY < 250:
-        offsetY += abs(player.y + offsetY - 250) / 30
+    if player.y + offsetY < 350:
+        offsetY += abs(player.y + offsetY - 350) / 30
 
     if player.y + offsetY > SCREEN_HEIGHT:
         run = False
-
-
-    # Dodawanie platform
-    if player.y < 1200 - 100 * Platform.counter:
-        posX = random.randint(10 + LEFT_LIMIT, RIGHT_LIMIT - Platform.image.get_width() - 10)
-        posY = 500 - 100 * Platform.counter
-        platformList.append(Platform(posX, posY))
-
-    # Usuwanie platform
-    for platform in platformList:
-        if platform.y + offsetY > SCREEN_HEIGHT:
-            platformList.remove(platform)
-            score += 10
-
-
     hurryUpTextPosY -= 1
 
-
+    Platform.manage_platforms()
 
     # Rysowane obiektów na ekranie
     draw_game()
